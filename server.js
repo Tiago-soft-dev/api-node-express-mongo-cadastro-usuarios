7cbbd501a8a50a937bf31d1c6403215e64447839
tiago-soft-dev <tiago.soft.dev@gmail.com>
2024-09-18 19:40:23 +0000

    Configuração Inicial

---
+++ server.js
@@ -0,0 +1,20 @@
+import express from "express";
+
+const app = express();
+
+app.use(express.json());
+
+const users = [];
+
+app.get("/usuarios", (req, res) => {
+  res.json(users);
+});
+
+app.post('/usuarios', (req, res)=>{
+  users.push(req.body)
+  res.status(201).json(req.body)
+})
+
+app.listen(3000, () => {
+  console.log("servidor online");
+});
